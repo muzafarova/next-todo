3cac1346174c85b8f7a2bffe752d7d3bc3cbc8f6
muzafarova <alisapsee@gmail.com>
2025-02-10 12:56:29 +0000

    Add docs page placeholder

---
+++ app/docs/page.tsx
@@ -0,0 +1,13 @@
+export default function Docs() {
+  return (
+    <div>
+      <h1>helllooooo</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi tempora
+        tenetur expedita amet blanditiis, voluptas fugiat quo deleniti eos
+        obcaecati reiciendis molestias vitae nam molestiae eligendi, sit quas
+        incidunt. Et?
+      </p>
+    </div>
+  )
+}
