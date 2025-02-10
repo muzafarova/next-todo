001a73580462f21bbf2ab63575eb79bebd3d7df6
muzafarova <alisapsee@gmail.com>
2025-02-10 12:57:55 +0000

    Style docs page

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -1,8 +1,8 @@
 export default function Docs() {
   return (
-    <div>
-      <h1>helllooooo</h1>
-      <p>
+    <div className="p-8 ">
+      <h1 className="text-3xl">helllooooo</h1>
+      <p className="max-w-prose my-8">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi tempora
         tenetur expedita amet blanditiis, voluptas fugiat quo deleniti eos
         obcaecati reiciendis molestias vitae nam molestiae eligendi, sit quas
