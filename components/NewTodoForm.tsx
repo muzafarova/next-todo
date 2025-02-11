f77c75e1180ac588b53677919be3046e57212e17
muzafarova <alisapsee@gmail.com>
2025-02-11 12:25:11 +0000

    Tweak form styles

--- components/NewTodoForm.tsx
+++ components/NewTodoForm.tsx
@@ -2,12 +2,12 @@ import { newTodo } from '@/utils/actions'
 
 export default function NewTodoForm() {
   return (
-    <form action={newTodo} className="flex gap-1">
+    <form action={newTodo} className="flex gap-1 text-lg">
       <input
         name="content"
         type="text"
         placeholder="Next task"
-        className="rounded py-1 px-2 flex-1 border-black/25 text-black bg-white"
+        className="rounded py-1 px-2 flex-1 border-black/25 border text-black bg-white"
         required
       />
       <button type="submit" className="button">
